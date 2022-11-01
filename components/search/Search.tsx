d8a9a9a17908855e3b79ba9d09e90144e63da343
cbm94 <cbm94@cornell.edu>
2022-11-01 12:03:13 -0400

    initial commit

---
+++ components/search/Search.tsx
@@ -0,0 +1,47 @@
+import { Button, Heading, HStack, Input, Spinner, VStack } from "@chakra-ui/react"
+import { addDoc, collection } from "firebase/firestore"
+import { FormEventHandler, useState } from "react"
+import { Location } from "../../types"
+import { db } from "../../util/firebase"
+import LocationList from "./LocationList"
+import locationList from "../../locationList"
+
+
+
+const SearchHeading = () => (
+  <Heading
+    as="h1"
+    w="fit-content"
+    bgGradient="linear(to-r, cyan.700, purple.500)"
+    bgClip="text"
+    lineHeight={1.33}
+  >
+    Housing Search
+  </Heading>
+)
+
+const Search = () => {
+  const [input, setInput] = useState("")
+
+  const searchLocation = (e: { target: { value: string } }) => {
+    setInput(e.target.value)
+  }
+
+  const locations = locationList.filter(x => input.length && (x.name.toLowerCase().includes(input.toLowerCase()) || x.region.toLowerCase().includes(input.toLowerCase())))
+
+  return (
+    <VStack spacing={4}>
+      <SearchHeading />
+      <Input
+        value={input}
+        type="text"
+        placeholder="Toni Morrison Hall..."
+        onChange={searchLocation}
+      />
+      {locations ? <LocationList locations={locations} /> : <Spinner />}
+    </VStack>
+      
+  )
+}
+
+export default Search
---
+++ types/index.ts
@@ -0,0 +1,21 @@
+export type Location = {
+  id: string
+  name: string
+  region: string
+  address: string
+  img: string
+  description: string
+  amenities: string[]
+}
+
+export type Review = {
+  id: string
+  date: string
+  locationID: string
+  author: string
+  recommend: boolean
+  pros: string[]
+  cons: string[]
+  rating: number
+  text: string
+}
